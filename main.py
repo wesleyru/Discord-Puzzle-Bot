80a99bcef5cdc76523b3563ea5cfee166c643ae5
wesleyru <ru.wesley@gmail.com>
2022-01-12 13:49:53 +0000

    Updated to support setup in repl.it

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from config import api_key, drive_id, template_key
 import discord
 import os
-from discord.ext.commands import Bot
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+
+drive_key = os.environ['GDRIVE_KEY']
+template_key = os.environ['GTEMPLATE_KEY']
+credentials = {
+  'type': os.environ['type'],
+  'project_id': os.environ['project_id'],
+  'private_key_id': os.environ['private_key_id'],
+  'private_key': os.environ['private_key'],
+  'client_email': os.environ['client_email'],
+  'client_id': os.environ['client_id'],
+  'auth_uri': os.environ['auth_uri'],
+  'token_uri': os.environ['token_uri'],
+  'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
+  'client_x509_cert_url': os.environ['client_x509_cert_url']
+}
 
 client = discord.Client()
-
-gc = gspread.service_account(filename=api_key)
+gc = gspread.service_account_from_dict(credentials)
 
 @client.event
 async def on_ready():
@@ -22,10 +33,10 @@ async def on_message(message):
         name = message.content.replace('$create ', '')
         gc.copy(template_key, title=name, copy_permissions=True)
         ss = gc.open(name)
-        link = "https://docs.google.com/spreadsheets/d/" + ss.id
+        link = 'https://docs.google.com/spreadsheets/d/' + ss.id
         await message.channel.send('New Spreadsheet Created Here: ' + link)
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('DISCORD_KEY'))
 
 
 
