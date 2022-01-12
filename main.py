992c9e840b9b6180e60a91750c62bb6db68df0f6
Wesley Ru <ru.wesley@gmail.com>
2022-01-12 09:43:28 -0500

    Standardize New Line Characters

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ credentials = {
   'type': os.environ['type'],
   'project_id': os.environ['project_id'],
   'private_key_id': os.environ['private_key_id'],
-  'private_key': os.environ['private_key'],
+  'private_key': os.environ['private_key'].replace('\\n','\n'),
   'client_email': os.environ['client_email'],
   'client_id': os.environ['client_id'],
   'auth_uri': os.environ['auth_uri'],
